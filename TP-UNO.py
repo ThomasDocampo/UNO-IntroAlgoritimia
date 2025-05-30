3c7471f75c9bc014969ba55b05e0a9147b5161f2
ThomasDocampo <83998404+ThomasDocampo@users.noreply.github.com>
2025-05-29 21:21:30 -0300

    Update TP-UNO.py
    agrego repartir e imprimir. Tambien deje un mazo pero medio chico

--- TP-UNO.py
+++ TP-UNO.py
@@ -1,4 +1,33 @@
-print("hola")
+import random
+
+'''
 
 
-print("Probando")
+'''
+
+mazo = ["1 ROJO", "2 ROJO", "3 ROJO", "4 ROJO","5 ROJO"]
+
+
+mazoPC = []
+mazoUsuario = []
+
+def repartir(cant,mazo):
+     lista = []
+     while cant > 0 :
+        lista.append(mazo[random.randint(0, len(mazo)-1)])
+        cant= cant -1
+     return lista
+    
+def mostrarMazo(msg, mazo) :
+    print(msg)
+    for i in range(0,len(mazo)) :
+        print(i+1, " -> ",mazo[i])
+    
+        
+mazoPC = repartir(7,mazo)
+mazoUsuario = repartir(7,mazo)
+mostrarMazo("el mazo de la pc es",mazoPC)
+
+mostrarMazo("tu mazo es",mazoUsuario)
+
+
