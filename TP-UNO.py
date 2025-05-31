e286ad772a4a9d04622c2878ea9e085b6eac5cd1
Lgoyer <lugoyer@gmail.com>
2025-05-30 23:06:13 -0300

    Update TP-UNO.py

--- TP-UNO.py
+++ TP-UNO.py
@@ -7,31 +7,27 @@ Funcion tirar , validar jugada,
 
 mazo = ["1 ROJO", "2 ROJO", "3 ROJO", "4 ROJO","5 ROJO"] #Otro planteo?
 
-
 mazoPC = []
 mazoUsuario = []
-
+#Funcion para repartir las cartas a los jugadores
 def repartir(cant,mazo):
      lista = []
      while cant > 0 :
         lista.append(mazo[random.randint(0, len(mazo)-1)])
         cant= cant -1
      return lista
-    
-def mostrarMazo(msg, mazo) :
+
+#Funcion para visualizar las cartas del jugador
+def mostrarMazo(msg, mazo):
     print(msg)
-    for i in range(0,len(mazo)) :
+    for i in range(0,len(mazo)):
         print(i+1, " -> ",mazo[i])
 
 #Programa principal
-#While para manejar los turnos
-    
-        
 mazoPC = repartir(7,mazo)
 mazoUsuario = repartir(7,mazo)
-mostrarMazo("el mazo de la pc es",mazoPC)
+mostrarMazo("El mazo de la pc es: ",mazoPC)
 
-
-mostrarMazo("tu mazo es",mazoUsuario)
+mostrarMazo("Tu mazo es: ",mazoUsuario)
 
 
