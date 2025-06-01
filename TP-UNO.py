bc55c21e91556c4dacadd105843743e6539067d1
ThomasDocampo <83998404+ThomasDocampo@users.noreply.github.com>
2025-06-01 19:40:37 -0300

    Update TP-UNO.py
    agrego la parte de jugar del usuario

--- TP-UNO.py
+++ TP-UNO.py
@@ -8,6 +8,7 @@ mazo = [[1,"ROJO"], [2,"ROJO"], [3,"ROJO"], [3,"ROJO"],[4,"ROJO"],[1,"AZUL"], [2
 
 mazoPC = []
 mazoUsuario = []
+cartaEnJuego = []
 #Funcion para repartir las cartas a los jugadores
 def repartir(cant,mazo):
      lista = []
@@ -17,16 +18,63 @@ def repartir(cant,mazo):
      return lista
 
 #Funcion para visualizar las cartas del jugador
-def mostrarMazo(msg, mazo):
-    print(msg)
+def mostrarMazo(mazo):
     for i in range(0,len(mazo)):
         print(i+1, " -> ",mazo[i])
-
+        
+def validarCarta (cartaEnJuego, cartaUsuario):
+      check = False
+      print (("aca estoy viendo que onda dcesde la funcion validar carta carta en juego ->"), cartaEnJuego)
+      print (("aca estoy viendo que onda dcesde la funcion validar carta jugador ->"), cartaUsuario)
+      if (cartaEnJuego[0] == cartaUsuario[0] or cartaEnJuego[1] == cartaUsuario[1]):
+          check = True
+      return check
+      
+def turnoUsuario (mazoUsuario, mazoGeneral, cartaEnJuego):
+    salir = False
+    tomoUnaCarta = False
+    opcion = -1
+    msgOpcion0 = "0  -> Tomar una carta"
+    print("es tu turno! Elegí una opcion o carta del mazo para jugar!")
+    while salir == False :
+      print (msgOpcion0)
+      mostrarMazo(mazoUsuario)
+      opcion = int(input("Elija una opcion:"))
+      if opcion < 0 or opcion > len(mazoUsuario)+1:
+           print ("opcion no valida!")
+      elif opcion == 0 and tomoUnaCarta == False:
+          print ( "el usuario toma una carta" )
+          mazoUsuario = mazoUsuario + repartir(1,mazoGeneral)
+          tomoUnaCarta = True
+          msgOpcion0 = "0  -> Pasar turno"
+      elif opcion == 0 and tomoUnaCarta == True :
+          salir = True
+          opcion = -1
+      else :
+          opcion = opcion-1
+          if(validarCarta(cartaEnJuego, mazoUsuario[opcion])):
+              cartaEnJuego = mazoUsuario[opcion]
+              del mazoUsuario[opcion]
+              salir = True
+          else:
+              print("no es una carta valida amiguin")
+              print ("la carta en juego es: ", cartaEnJuego)
+          
+          
+    return cartaEnJuego, mazoUsuario
 #Programa principal
+
 mazoPC = repartir(7,mazo)
 mazoUsuario = repartir(7,mazo)
-mostrarMazo("El mazo de la pc es: ",mazoPC)
+indiceUsuario = 0
+cartaEnJuego = repartir(1,mazo)[0]
 
-mostrarMazo("Tu mazo es: ",mazoUsuario)
+while len(mazoPC) > 0 and len(mazoUsuario) > 0 :
+    print ("la carta en juego es: ", cartaEnJuego)
+    cartaEnJuego, mazoUsuario= turnoUsuario(mazoUsuario,mazo,cartaEnJuego)
+    
+        
+       
+           
 
-
+mostrarMazo("mazoUsuario")
